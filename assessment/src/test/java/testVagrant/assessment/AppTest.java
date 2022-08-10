c8be1b52ddb5234ab5276237d99b2e719be18beb
siva ganesh <siva ganesh@LAPTOP-IPB288HR>
2022-08-10 08:10:04 +0530

    AddProject

---
+++ assessment/src/test/java/testVagrant/assessment/AppTest.java
@@ -0,0 +1,21 @@
+package testVagrant.assessment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println( true );
+    }
+}
