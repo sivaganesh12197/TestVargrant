e7a93e5679aad1798c97dc7ce2634ca69f3c2a34
siva ganesh <siva ganesh@LAPTOP-IPB288HR>
2022-08-10 19:35:57 +0530

    AssementTestsAdded

--- assessment/src/main/java/testVagrant/assessment/App.java
+++ assessment/src/main/java/testVagrant/assessment/App.java
@@ -1,13 +1,37 @@
 package testVagrant.assessment;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static List<String> getSpecificPlayerInfo(String key  ) throws FileNotFoundException, IOException, ParseException 
     {
-        System.out.println( "Hello World!" );
+    	 String file = "json/playerList.json";
+    	 JSONParser parser = new JSONParser();
+         Object obj = parser.parse(new FileReader(file));
+         JSONObject jsonObject = (JSONObject) obj;
+         JSONArray players = (JSONArray) jsonObject.get("player");
+         List<String> playerinfo = new ArrayList<String>();
+         for (Object o : players)
+         {
+           JSONObject player = (JSONObject) o;
+           playerinfo.add(player.get(key).toString());
+           System.out.println(player.get(key));
+         }
+         return playerinfo;
     }
 }
--- assessment/src/test/java/testVagrant/assessment/AppTest.java
+++ assessment/src/test/java/testVagrant/assessment/AppTest.java
@@ -2,7 +2,19 @@ package testVagrant.assessment;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
 import org.testng.annotations.Test;
+import testVagrant.assessment.*;
 
 
 /**
@@ -12,10 +24,25 @@ public class AppTest
 {
     /**
      * Rigorous Test :-)
+     * @throws FileNotFoundException 
+     * @throws IOException 
+     * @throws ParseException 
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void VerifyPlayerCount() throws FileNotFoundException, IOException, ParseException 
     {
-        System.out.println( true );
+    	List<String> playerCountries = new ArrayList<String>();
+    	playerCountries = App.getSpecificPlayerInfo("country"); 
+    	int n = (int) playerCountries.stream().filter(s->!s.equals("India")).count();
+    	Assert.assertTrue(n==4);
+    }
+    
+    @Test
+    public void VerifyWicketKeeper() throws IOException, ParseException
+    {
+    	List<String> playerCountries = new ArrayList<String>();
+    	playerCountries = App.getSpecificPlayerInfo("role"); 
+    	int m = (int) playerCountries.stream().filter(s->s.equalsIgnoreCase("Wicket-keeper")).count();
+    	Assert.assertTrue(m>=1);
     }
 }
